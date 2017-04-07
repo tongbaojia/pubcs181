e191de09d8700a580ca18e32d8719f504c281263
ydovzhenko <dovzhenko@g.harvard.edu>
2017-04-07 01:46:50 -0400

    Playing with SVD and k nearest neighbors.
    Started the writeup

--- p3/predict_SVD_v1.py
+++ p3/predict_SVD_v1.py
@@ -1,5 +1,5 @@
 #First, perform SVD to reduce dimensions of the data
-#Then, predict using average values over g nearest neighbors
+#Then,predict by projecting user eigenvectors onto artist eigenvectors
 
 import numpy as np
 import csv
@@ -42,7 +42,6 @@ with open(train_file, 'r') as train_fh:
         if count >num_lines:
             break
 #print(count)
-
 #get user means to rescale
 user_total = {}
 user_means = {}
@@ -54,8 +53,7 @@ for user, user_data in train_data.iteritems():
     user_total[user] = np.sum(np.array(user_plays))
     user_means[user] = np.mean(np.array(user_plays))
     user_medians[user] = np.median(np.array(user_plays))
-    
-    #To make a sparse matrix, index users and artists sequentially using data in artists, profiles
+#To make a sparse matrix, index users and artists sequentially using data in artists, profiles
 
 user_file = 'profiles.csv'
 artist_file = 'artists.csv'
@@ -102,45 +100,23 @@ train_mat = scipy.sparse.csr_matrix(train_mat)
 print train_mat.shape
 
 
-from scipy.spatial import KDTree
-
-group_size_vals = np.array([2,4,8])
-k_vals = np.array([6])
-acc = np.zeros(group_size_vals.shape)
-U,S,V=scipy.sparse.linalg.svds(train_mat, k = k_vals[0])
-test_mat = lil_matrix((len(user_num),len(artist_num)), dtype=np.float)
-pred_mat = lil_matrix((len(user_num),len(artist_num)), dtype=np.float)
-for i in range(group_size_vals.size):
-    group_size = group_size_vals[i]
+k_vals = np.array([1400])
+acc = np.zeros(k_vals.shape)
+for i in range(k_vals.size):
     #Perform SVD-like decomposition of the training data
-    #U,S,V=scipy.sparse.linalg.svds(train_mat, k = k_vals[i])
-    
-    tree = KDTree(U)
-    
+    U,S,V=scipy.sparse.linalg.svds(train_mat, k = k_vals[i])
+    #do the same to the test data
+
+    test_mat = lil_matrix((len(user_num),len(artist_num)), dtype=np.float)
+    pred_mat = lil_matrix((len(user_num),len(artist_num)), dtype=np.float)
+
     for user in test_data:
-        #print(user_id)
         user_id = user_num[user]
-        if user_id%100 == 0:       
-            print(user_id)
-        dists, top_inds = tree.query(U[user_id,:], k = group_size)
-        #find closest neighboring users
-        sum_user = train_mat[top_inds[1:], :].sum(0)
-        #dists =  np.linalg.norm( np.subtract(U[user_id,:], U), axis = 1)
-        #top_inds = np.argpartition(dists, group_size)[:group_size]
-        #group_avg = np.mean(U[top_inds, :], axis = 0)         
+        user_row = U[user_id,:]
         for artist in test_data[user]:
             artist_id = artist_num[artist]                      
             test_mat[user_id, artist_id] = test_data[user][artist]
-            sum_pred = sum_user[0,artist_id]
-            #if sum_pred>0:
-            pred_mat[user_id, artist_id] =np.true_divide(sum_pred,group_size)*np.float(user_total[user])
-            #pred_mat[user_id, artist_id] = (user_medians[user]+(group_size)*pred_mat[user_id, artist_id])/(group_size+1.0)
-            #pred_mat[user_id, artist_id] =np.true_divide(sum_pred,(train_mat[top_inds, artist_id]!=0).sum(0))*np.float(user_total[user])
-                #pred_mat[user_id, artist_id] = (user_medians[user]+pred_mat[user_id, artist_id])/2.0
-            #else:
-                #pred_mat[user_id, artist_id] = user_medians[user]
-            #pred_mat[user_id, artist_id] =np.true_divide(train_mat[top_inds, artist_id].sum(0),(train_mat[top_inds, artist_id]!=0).sum(0))*np.float(user_total[user])
-            #pred_mat[user_id, artist_id] = np.matmul(np.matmul(group_avg, np.diag(S)), V[:,artist_id])*np.float(user_total[user])
+            pred_mat[user_id, artist_id] = np.matmul(np.matmul(user_row, np.diag(S)), V[:,artist_id])*np.float(user_total[user])
             #predict number of plays
 
     # print test_mat.shape
@@ -149,14 +125,3 @@ for i in range(group_size_vals.size):
     num_entries = sum(len(v) for v in test_data.itervalues())
     acc[i] = np.sum(np.absolute(np.subtract(pred_mat,test_mat)))/num_entries
     print(acc)
-    
-
-plt.clf()
-fig = plt.figure()
-plt.plot(group_size_vals,acc)
-plt.xlabel('Number of nearest neighbors')
-plt.ylabel('Mean absolute error')
-plt.title('k = '+str(k_vals[0]))
-plt.show()
-fig.savefig('SVD_group_'+str(group_size_vals[-1])+'.png')
-    
